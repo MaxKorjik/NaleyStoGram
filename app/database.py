6ec06802ebd69cff40bfbc37a2b31f7094294a0f
MaxKorjik <koshchinmaxim06@gmail.com>
2026-02-10 22:31:41 +0200

    added cors

--- app/database.py
+++ app/database.py
@@ -3,6 +3,5 @@ from pymongo import MongoClient
 MONGO_URI = 'mongodb+srv://ytgreik_db_user:9AaCxiuZyIVqQ2RO@naleystogram.v98iit4.mongodb.net/?appName=NaleyStoGram'
 client = MongoClient(MONGO_URI)
 
-print(client.list_database_names())
 db = client.naleystogram
 users = db.users
--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from .auth import router
 
 app = FastAPI()
 app.include_router(router=router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
 
 @app.get('/')
 def home():
